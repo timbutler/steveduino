9d64edbee45a5e9efeb7f68d3aae1a8b815808a9
Tim Butler <tim.butler.au@gmail.com>
2013-10-23 22:21:01 +1000

    The Arduino code!

---
+++ steves_eyes/steves_eyes.ino
@@ -0,0 +1,97 @@
+/*
+  StevePIREyes
+  Uses a basic PIR sensor to control LED eyes for Steve the skeleton
+ 
+  @author: Tim Butler
+  @gitrepo: https://github.com/timbutler/steveduino
+ */
+ 
+// Variables
+// -----------------------
+
+int pirInputPin = 5;
+int ledOutputPin = 13;
+int pirState = LOW;
+int startDelay = 3 * 1000;
+int retriggerDelay = 20 * 1000;
+
+// Allow time for the PIR to calibrate at startup
+void startupDelay() {
+  delay (startDelay);
+}
+
+
+
+void ledStartup() {
+  analogWrite(ledOutputPin, 0);         
+  delay(100);
+  analogWrite(ledOutputPin, 100);         
+  delay(100);
+  analogWrite(ledOutputPin, 0);         
+  delay(100);
+  analogWrite(ledOutputPin, 100);         
+  delay(100);
+  analogWrite(ledOutputPin, 0);         
+  delay(100);
+  analogWrite(ledOutputPin, 100);         
+  delay(100);
+  analogWrite(ledOutputPin, 0);         
+}
+
+void ledFadeIn() {
+  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) { 
+    analogWrite(ledOutputPin, fadeValue);         
+    delay(5);
+  }
+}
+
+void ledFadeOut() {
+  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) { 
+      analogWrite(ledOutputPin, fadeValue);         
+    delay(1);                            
+  }  
+}
+
+void ledFadeInOut() {
+  ledFadeIn();
+  ledFadeOut();
+}
+
+void freakyEyes() {
+  ledFadeInOut();
+  ledFadeInOut();
+  ledFadeInOut();
+  ledFadeIn();
+  analogWrite(ledOutputPin, 255);
+  //delay(5 * 1000);
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+  pinMode(pirInputPin, INPUT);
+  pinMode(ledOutputPin, OUTPUT);
+  ledStartup();
+  Serial.println("Steves Eyes 0.1");
+  Serial.println("=============================");
+  Serial.print("PIR startup delay...............");
+  startupDelay();
+  Serial.println("done!");
+}
+
+void loop() {
+
+  if(digitalRead(pirInputPin) == LOW){ 
+       analogWrite(ledOutputPin, 0); 
+    Serial.println("I'm low :(");
+  }
+  else {
+    analogWrite(ledOutputPin, 255);
+    freakyEyes();
+    delay(2000);
+  }
+  
+  //int inputVal = digitalRead(pirInputPin);
+  //Serial.println(inputVal);
+  delay(500);        // delay in between reads for stability
+}
